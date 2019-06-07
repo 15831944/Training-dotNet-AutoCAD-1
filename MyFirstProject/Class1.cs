33c1885ed37f8377a8ae0310a0a9822f9d7aeaac
Mr-Saxobeat <weiglasribeiro@gmail.com>
2019-06-07 16:47:53 -0300

    Upload rigth project
    Now, this project is with the corret .Net target, .Net framework 4.8

---
+++ MyFirstProject/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+[assembly: CommandClass(typeof(MyFirstProject1.Class1))]
+
+namespace MyFirstProject1
+{
+    public class Class1
+    {
+        [CommandMethod("AdskGreeting")]
+        public void AdskGreeting()
+        {
+            // Get the current document and database, and start a transaction
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Starts a new transaction with the Transaction Manager
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the block table record for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                            OpenMode.ForRead) as BlockTable;
+
+                // Open the block table record model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                    OpenMode.ForWrite) as BlockTableRecord;
+
+                // Creates a new MText object and assigns it a location,
+                // text value and text style
+                using (MText objText = new MText())
+                {
+                    // Specify the insertion point of the Mtext object
+                    objText.Location = new Autodesk.AutoCAD.Geometry.Point3d(2, 2, 0);
+
+                    // Set the text string for the MText object
+                    objText.Contents = "Greetins, Welcome to AutoCA .NET";
+
+                    // Set the text style for the MText object
+                    objText.TextStyleId = acCurDb.Textstyle;
+
+                    // Appends the new MText object model space
+                    acBlkTblRec.AppendEntity(objText);
+
+                    // Appends the new MText object to the active transaction
+                    acTrans.AddNewlyCreatedDBObject(objText, true);
+                }
+
+                // Saves the change to the database and closes the transaction
+                acTrans.Commit();
+            }
+        }
+    }
+}
