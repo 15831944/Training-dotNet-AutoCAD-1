34aa5eb46ff812d05b0331a98ea04a6987bc3bd0
Mr-Saxobeat <weiglasribeiro@gmail.com>
2019-06-12 15:30:34 -0300

    Add "Show window state" project

---
+++ ShowWindowState/ShowWindowState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ShowWindowState
+{
+    public class ShowWindowState
+    {
+        [CommandMethod("ShowCurrentWindowState")]
+        public static void ShowCurrentWindowState()
+        {
+            System.Windows.Forms.MessageBox
+                .Show("The application window is " +
+                       Application.MainWindow.WindowState.ToString());
+        }
+    }
+}
